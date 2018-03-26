3aefae77fda368af95953aaf259d6d98d426bef9
John Marshall <john.marshall@shroogal.com.au>
2018-03-26 16:37:39 +1100

    Fix formatting, efficiency in Hex conversions

--- addons/binding/org.openhab.binding.broadlink/src/main/java/org/openhab/binding/broadlink/internal/Hex.java
+++ addons/binding/org.openhab.binding.broadlink/src/main/java/org/openhab/binding/broadlink/internal/Hex.java
@@ -9,21 +9,18 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import javax.xml.bind.DatatypeConverter;
 
-public class Hex
-{
+public class Hex {
 
-    public Hex()
-    {
-    }
+    private static final Pattern validPattern = Pattern.compile("0000( +[0-9A-Fa-f][0-9A-Fa-f][0-9A-Fa-f][0-9A-Fa-f])+");
+		private static final String HEXES = "0123456789ABCDEF";
 
-    public static String decodeMAC(byte mac[])
-    {
-        if(mac == null)
-            return null;
+    public static String decodeMAC(byte mac[]) {
+        if (mac == null) return null;
+
         StringBuilder sb = new StringBuilder(18);
         for(int i = 5; i >= 0; i--)
         {
-            if(sb.length() > 0)
+            if (sb.length() > 0)
                 sb.append(':');
             sb.append(String.format("%02x", new Object[] {
                 Byte.valueOf(mac[i])
@@ -33,25 +30,20 @@ public class Hex
         return sb.toString();
     }
 
-    public static boolean isHexCode(String code)
-    {
-        Pattern pattern = Pattern.compile("0000( +[0-9A-Fa-f][0-9A-Fa-f][0-9A-Fa-f][0-9A-Fa-f])+");
-        return pattern.matcher(code).find();
+    public static boolean isHexCode(String code) {
+        return validPattern.matcher(code).find();
     }
 
-    public static byte[] convertHexToBytes(String code)
-    {
+    public static byte[] convertHexToBytes(String code) {
         return DatatypeConverter.parseHexBinary(code);
     }
 
-    public static byte[] fromHexString(String hex)
-    {
-        if(hex.length() % 2 != 0)
-            throw new IllegalArgumentException("Input string must contain an even number of characters");
+    public static byte[] fromHexString(String hex) {
+        if (hex.length() % 2 != 0) throw new IllegalArgumentException("Input string must contain an even number of characters");
+
         byte result[] = new byte[hex.length() / 2];
         char bytes[] = hex.toCharArray();
-        for(int i = 0; i < bytes.length; i += 2)
-        {
+        for(int i = 0; i < bytes.length; i += 2) {
             StringBuilder curr = new StringBuilder(2);
             curr.append(bytes[i]).append(bytes[i + 1]);
             result[i / 2] = (byte)Integer.parseInt(curr.toString(), 16);
@@ -60,18 +52,14 @@ public class Hex
         return result;
     }
 
-    public static String toHexString(byte raw[])
-    {
-        String HEXES = "0123456789ABCDEF";
-        if(raw == null)
-            return null;
+    public static String toHexString(byte raw[]) {
+				if (raw == null) return null;
         StringBuilder hex = new StringBuilder(2 * raw.length);
         byte abyte0[];
         int j = (abyte0 = raw).length;
-        for(int i = 0; i < j; i++)
-        {
+        for(int i = 0; i < j; i++) {
             byte b = abyte0[i];
-            hex.append("0123456789ABCDEF".charAt((b & 0xf0) >> 4)).append("0123456789ABCDEF".charAt(b & 0xf));
+            hex.append(HEXES.charAt((b & 0xf0) >> 4)).append(HEXES.charAt(b & 0xf));
         }
 
         return hex.toString();
