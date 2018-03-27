ef0786335ee4191dc64bd952b70e9e5b1272bcac
John Marshall <john.marshall@shroogal.com.au>
2018-03-27 12:27:17 +1100

    Fix formatting

--- addons/binding/org.openhab.binding.broadlink/src/main/java/org/openhab/binding/broadlink/config/BroadlinkDeviceConfiguration.java
+++ addons/binding/org.openhab.binding.broadlink/src/main/java/org/openhab/binding/broadlink/config/BroadlinkDeviceConfiguration.java
@@ -6,41 +6,40 @@
 package org.openhab.binding.broadlink.config;
 
 
-public class BroadlinkDeviceConfiguration
-{
+public class BroadlinkDeviceConfiguration {
+    private String ipAddress;
+    private int port;
+    private String mac;
+    private int pollingInterval;
+    private String mapFilename;
+    private String authorizationKey;
+    private String iv;
 
-    public BroadlinkDeviceConfiguration()
-    {
+    public BroadlinkDeviceConfiguration() {
         pollingInterval = 30;
     }
 
-    public String getIpAddress()
-    {
+    public String getIpAddress() {
         return ipAddress;
     }
 
-    public void setIpAddress(String ipAddress)
-    {
+    public void setIpAddress(String ipAddress) {
         this.ipAddress = ipAddress;
     }
 
-    public void setPort(int port)
-    {
+    public void setPort(int port) {
         this.port = port;
     }
 
-    public int getPort()
-    {
+    public int getPort() {
         return port;
     }
 
-    public void setMAC(String mac)
-    {
+    public void setMAC(String mac) {
         this.mac = mac;
     }
 
-    public byte[] getMAC()
-    {
+    public byte[] getMAC() {
         byte configMac[] = new byte[6];
         String elements[] = mac.split(":");
         for(int i = 0; i < 6; i++)
@@ -52,56 +51,40 @@ public class BroadlinkDeviceConfiguration
         return configMac;
     }
 
-    public int getPollingInterval()
-    {
+    public int getPollingInterval() {
         return pollingInterval;
     }
 
-    public void setPollingInterval(int pollingInterval)
-    {
+    public void setPollingInterval(int pollingInterval) {
         this.pollingInterval = pollingInterval;
     }
 
-    public String getMapFilename()
-    {
+    public String getMapFilename() {
         return mapFilename;
     }
 
-    public void setMapFilename(String mapFilename)
-    {
+    public void setMapFilename(String mapFilename) {
         this.mapFilename = mapFilename;
     }
 
-    public String getAuthorizationKey()
-    {
+    public String getAuthorizationKey() {
         return authorizationKey;
     }
 
-    public void setAuthorizationKey(String authorizationKey)
-    {
+    public void setAuthorizationKey(String authorizationKey) {
         this.authorizationKey = authorizationKey;
     }
 
-    public String getIV()
-    {
+    public String getIV() {
         return iv;
     }
 
-    public void setIV(String iv)
-    {
+    public void setIV(String iv) {
         this.iv = iv;
     }
 
-    public String toString()
-    {
+    public String toString() {
         return (new StringBuilder("BroadlinkDeviceConfiguration [ipAddress=")).append(ipAddress).append(", port=").append(port).append(", mac=").append(mac).append(", pollingInterval=").append(pollingInterval).append(", mapFilename=").append(mapFilename).append(", authorizationKey=").append(authorizationKey).append(", iv=").append(iv).append("]").toString();
     }
 
-    private String ipAddress;
-    private int port;
-    private String mac;
-    private int pollingInterval;
-    private String mapFilename;
-    private String authorizationKey;
-    private String iv;
 }
