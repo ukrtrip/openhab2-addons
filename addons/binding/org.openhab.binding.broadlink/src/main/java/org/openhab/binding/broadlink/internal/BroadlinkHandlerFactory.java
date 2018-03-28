c31e0bcb97f7e63b2097f3e00168a3af08a1613f
John Marshall <john.marshall@shroogal.com.au>
2018-03-28 14:12:38 +1100

    Fix formatting of the handler factory

--- addons/binding/org.openhab.binding.broadlink/src/main/java/org/openhab/binding/broadlink/internal/BroadlinkHandlerFactory.java
+++ addons/binding/org.openhab.binding.broadlink/src/main/java/org/openhab/binding/broadlink/internal/BroadlinkHandlerFactory.java
@@ -35,63 +35,46 @@ import org.slf4j.LoggerFactory;
 @Component(service = ThingHandlerFactory.class, immediate = true, configurationPid = "binding.broadlink")
 @NonNullByDefault
 public class BroadlinkHandlerFactory extends BaseThingHandlerFactory {
+
     private final Logger logger = LoggerFactory.getLogger(BroadlinkHandlerFactory.class);
     private Map discoveryServiceRegs;
     private List channelTypes;
     private List channelGroupTypes;
 
-    public BroadlinkHandlerFactory()
-    {
+    public BroadlinkHandlerFactory() {
         discoveryServiceRegs = new HashMap();
         channelTypes = new CopyOnWriteArrayList();
         channelGroupTypes = new CopyOnWriteArrayList();
     }
 
-    public boolean supportsThingType(ThingTypeUID thingTypeUID)
-    {
+    public boolean supportsThingType(ThingTypeUID thingTypeUID) {
         return BroadlinkBindingConstants.SUPPORTED_THING_TYPES_UIDS.contains(thingTypeUID);
     }
 
-    protected ThingHandler createHandler(Thing thing)
-    {
+    protected ThingHandler createHandler(Thing thing) {
         ThingTypeUID thingTypeUID = thing.getThingTypeUID();
-        if(logger.isDebugEnabled())
-            logger.debug("Creating Thing handler for '{}'", thingTypeUID.getAsString());
-        if(thingTypeUID.equals(BroadlinkBindingConstants.THING_TYPE_RM))
-            return new BroadlinkRemoteModel2Handler(thing);
-        if(thingTypeUID.equals(BroadlinkBindingConstants.THING_TYPE_RM2))
-        {
-            if(logger.isDebugEnabled())
-                logger.debug("org.openhab.binding.broadlink.internal", "RM 2 handler requested created");
+        if(logger.isDebugEnabled()) logger.debug("Creating Thing handler for '{}'", thingTypeUID.getAsString());
+        if(thingTypeUID.equals(BroadlinkBindingConstants.THING_TYPE_RM)) return new BroadlinkRemoteModel2Handler(thing);
+        if(thingTypeUID.equals(BroadlinkBindingConstants.THING_TYPE_RM2)) {
+            if(logger.isDebugEnabled()) logger.debug("org.openhab.binding.broadlink.internal", "RM 2 handler requested created");
             return new BroadlinkRemoteModel2Handler(thing);
         }
-        if(thingTypeUID.equals(BroadlinkBindingConstants.THING_TYPE_RM3))
-        {
-            if(logger.isDebugEnabled())
-                logger.debug("org.openhab.binding.broadlink.internal", "RM 3 handler requested created");
+        if(thingTypeUID.equals(BroadlinkBindingConstants.THING_TYPE_RM3)) {
+            if(logger.isDebugEnabled()) logger.debug("org.openhab.binding.broadlink.internal", "RM 3 handler requested created");
             return new BroadlinkRemoteHandler(thing);
         }
-        if(thingTypeUID.equals(BroadlinkBindingConstants.THING_TYPE_A1))
-        {
-            if(logger.isDebugEnabled())
-                logger.debug("org.openhab.binding.broadlink.internal", "A1 handler requested created");
+        if(thingTypeUID.equals(BroadlinkBindingConstants.THING_TYPE_A1)) {
+            if(logger.isDebugEnabled()) logger.debug("org.openhab.binding.broadlink.internal", "A1 handler requested created");
             return new BroadlinkA1Handler(thing);
         }
-        if(thingTypeUID.equals(BroadlinkBindingConstants.THING_TYPE_MP1))
+        if(thingTypeUID.equals(BroadlinkBindingConstants.THING_TYPE_MP1)) return new BroadlinkStripModel1Handler(thing);
+        if(thingTypeUID.equals(BroadlinkBindingConstants.THING_TYPE_SP1)) return new BroadlinkSocketModel1Handler(thing);
+        if(thingTypeUID.equals(BroadlinkBindingConstants.THING_TYPE_SP2)) return new BroadlinkSocketModel2Handler(thing);
+        if(thingTypeUID.equals(BroadlinkBindingConstants.THING_TYPE_SP3)) return new BroadlinkSocketModel3Handler(thing);
+        if(thingTypeUID.equals(BroadlinkBindingConstants.THING_TYPE_MP1)) return new BroadlinkStripModel1Handler(thing);
+        if(thingTypeUID.equals(BroadlinkBindingConstants.THING_TYPE_MP2)) {
             return new BroadlinkStripModel1Handler(thing);
-        if(thingTypeUID.equals(BroadlinkBindingConstants.THING_TYPE_SP1))
-            return new BroadlinkSocketModel1Handler(thing);
-        if(thingTypeUID.equals(BroadlinkBindingConstants.THING_TYPE_SP2))
-            return new BroadlinkSocketModel2Handler(thing);
-        if(thingTypeUID.equals(BroadlinkBindingConstants.THING_TYPE_SP3))
-            return new BroadlinkSocketModel3Handler(thing);
-        if(thingTypeUID.equals(BroadlinkBindingConstants.THING_TYPE_MP1))
-            return new BroadlinkStripModel1Handler(thing);
-        if(thingTypeUID.equals(BroadlinkBindingConstants.THING_TYPE_MP2))
-        {
-            return new BroadlinkStripModel1Handler(thing);
-        } else
-        {
+        } else {
             thingTypeUID.equals(BroadlinkBindingConstants.THING_TYPE_S1C);
             thingTypeUID.equals(BroadlinkBindingConstants.THING_TYPE_PIR);
             thingTypeUID.equals(BroadlinkBindingConstants.THING_TYPE_MAGNET);
@@ -99,21 +82,17 @@ public class BroadlinkHandlerFactory extends BaseThingHandlerFactory {
         }
     }
 
-    protected synchronized void removeHandler(ThingHandler thingHandler)
-    {
-        if(thingHandler instanceof BroadlinkControllerHandler)
-        {
+    protected synchronized void removeHandler(ThingHandler thingHandler) {
+        if(thingHandler instanceof BroadlinkControllerHandler) {
             ServiceRegistration serviceReg = (ServiceRegistration)discoveryServiceRegs.get(thingHandler.getThing().getUID());
-            if(serviceReg != null)
-            {
+            if(serviceReg != null) {
                 serviceReg.unregister();
                 discoveryServiceRegs.remove(thingHandler.getThing().getUID());
             }
         }
     }
 
-    private synchronized void registerBroadlinkDeviceDiscoveryService(BroadlinkControllerHandler broadlinkControllerHandler)
-    {
+    private synchronized void registerBroadlinkDeviceDiscoveryService(BroadlinkControllerHandler broadlinkControllerHandler) {
         BroadlinkDeviceDiscoveryService discoveryService = new BroadlinkDeviceDiscoveryService(broadlinkControllerHandler);
         discoveryServiceRegs.put(
             broadlinkControllerHandler.getThing().getUID(),
@@ -124,5 +103,4 @@ public class BroadlinkHandlerFactory extends BaseThingHandlerFactory {
             )
         );
     }
-
 }
