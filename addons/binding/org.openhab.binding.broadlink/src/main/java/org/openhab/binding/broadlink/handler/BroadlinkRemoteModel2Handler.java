fb9e1db8aa0cd24694434405e32bc5dab3928895
John Marshall <john.marshall@shroogal.com.au>
2018-04-23 10:06:15 +1000

    Fix file formatting for RM2 handler

--- addons/binding/org.openhab.binding.broadlink/src/main/java/org/openhab/binding/broadlink/handler/BroadlinkRemoteModel2Handler.java
+++ addons/binding/org.openhab.binding.broadlink/src/main/java/org/openhab/binding/broadlink/handler/BroadlinkRemoteModel2Handler.java
@@ -1,8 +1,3 @@
-// Decompiled by Jad v1.5.8g. Copyright 2001 Pavel Kouznetsov.
-// Jad home page: http://www.kpdus.com/jad.html
-// Decompiler options: packimports(3) 
-// Source File Name:   BroadlinkRemoteModel2Handler.java
-
 package org.openhab.binding.broadlink.handler;
 
 import java.util.Map;
@@ -12,58 +7,47 @@ import org.eclipse.smarthome.core.thing.*;
 import org.openhab.binding.broadlink.internal.Hex;
 import org.openhab.binding.broadlink.internal.Utils;
 
-// Referenced classes of package org.openhab.binding.broadlink.handler:
-//            BroadlinkRemoteHandler
+public class BroadlinkRemoteModel2Handler extends BroadlinkRemoteHandler {
 
-public class BroadlinkRemoteModel2Handler extends BroadlinkRemoteHandler
-{
-
-    public BroadlinkRemoteModel2Handler(Thing thing)
-    {
+    public BroadlinkRemoteModel2Handler(Thing thing) {
         super(thing);
     }
 
-    public void updateItemStatus()
-    {
-        if(getStatusFromDevice())
-        {
-            if(!isOnline())
+    public void updateItemStatus() {
+        if (getStatusFromDevice()) {
+            if (!isOnline()) {
                 updateStatus(ThingStatus.ONLINE);
-        } else
-        if(!isOffline())
-            updateStatus(ThingStatus.OFFLINE, ThingStatusDetail.COMMUNICATION_ERROR, (new StringBuilder("Could not control device at IP address ")).append(thingConfig.getIpAddress()).toString());
+						}
+        } else {
+						if (!isOffline()) {
+								updateStatus(ThingStatus.OFFLINE, ThingStatusDetail.COMMUNICATION_ERROR, (new StringBuilder("Could not control device at IP address ")).append(thingConfig.getIpAddress()).toString());
+						}
+				}
     }
 
-    public boolean getStatusFromDevice()
-    {
+    public boolean getStatusFromDevice() {
         byte payload[] = new byte[16];
         payload[0] = 1;
         byte message[] = buildMessage((byte)106, payload);
         sendDatagram(message, "RM2 device status");
         byte response[] = receiveDatagram("RM2 device status");
-        if(response != null)
-        {
+        if (response != null) {
             int error = response[34] | response[35] << 8;
-            if(error == 0)
-            {
+            if (error == 0) {
                 IvParameterSpec ivSpec = new IvParameterSpec(Hex.convertHexToBytes(thingConfig.getIV()));
                 Map properties = editProperties();
                 byte decodedPayload[] = Utils.decrypt(Hex.fromHexString((String)properties.get("key")), ivSpec, Utils.slice(response, 56, 88));
-                if(decodedPayload != null)
-                {
+                if (decodedPayload != null) {
                     float temperature = (float)((double)(decodedPayload[4] * 10 + decodedPayload[5]) / 10D);
                     updateState("temperature", new DecimalType(temperature));
                     return true;
-                } else
-                {
+                } else {
                     return false;
                 }
-            } else
-            {
+            } else {
                 return false;
             }
-        } else
-        {
+        } else {
             return false;
         }
     }
