d49cfbb775dbdfd9bc16367e0688bb75cc6633e0
John Marshall <john.marshall@shroogal.com.au>
2018-04-23 10:15:10 +1000

    Fix file formatting for Socket model 2 handler

--- addons/binding/org.openhab.binding.broadlink/src/main/java/org/openhab/binding/broadlink/handler/BroadlinkSocketModel2Handler.java
+++ addons/binding/org.openhab.binding.broadlink/src/main/java/org/openhab/binding/broadlink/handler/BroadlinkSocketModel2Handler.java
@@ -25,11 +25,13 @@ public class BroadlinkSocketModel2Handler extends BroadlinkSocketHandler {
     }
 
     public void handleCommand(ChannelUID channelUID, Command command) {
-        if (channelUID.getId().equals("powerOn"))
-            if (command == OnOffType.ON)
+        if (channelUID.getId().equals("powerOn")) {
+            if (command == OnOffType.ON) {
                 setStatusOnDevice(1);
-            else if (command == OnOffType.OFF)
+						} else if (command == OnOffType.OFF) {
                 setStatusOnDevice(0);
+						}
+				}
     }
 
     private void setStatusOnDevice(int status) {
@@ -41,52 +43,51 @@ public class BroadlinkSocketModel2Handler extends BroadlinkSocketHandler {
     }
 
     public boolean getStatusFromDevice() {
-        label0:
-        {
-            label1:
-            {
-                label2:
-                {
-                    byte payload[] = new byte[16];
-                    payload[0] = 1;
-                    try {
-                        byte message[] = buildMessage((byte) 106, payload);
-                        sendDatagram(message, "status for socket");
-                        byte response[] = receiveDatagram("status for socket");
-                        if (response == null)
-                            break label0;
-                        int error = response[34] | response[35] << 8;
-                        if (error != 0)
-                            break label1;
-                        IvParameterSpec ivSpec = new IvParameterSpec(Hex.convertHexToBytes(thingConfig.getIV()));
-                        Map properties = editProperties();
-                        byte decodedPayload[] = Utils.decrypt(Hex.fromHexString((String) properties.get("key")), ivSpec, Utils.slice(response, 56, 88));
-                        if (decodedPayload == null)
-                            break label2;
-                        if (payload[4] == 1)
-                            updateState("powerOn", OnOffType.ON);
-                        else
-                            updateState("powerOn", OnOffType.OFF);
-                    } catch (Exception ex) {
-                        logger.error("Exception while getting status from device", ex);
-                        return false;
-                    }
-                    return true;
-                }
-                return false;
-            }
-            return false;
-        }
-        return false;
+				try {
+						byte payload[] = new byte[16];
+						payload[0] = 1;
+						byte message[] = buildMessage((byte) 106, payload);
+						sendDatagram(message, "status for socket");
+						byte response[] = receiveDatagram("status for socket");
+						if (response == null) {
+								logError("null response from model 2 status request");
+								return false;
+						}
+						int error = response[34] | response[35] << 8;
+						if (error != 0) {
+								logError("Error response from model 2 status request; code: " + error);
+								return false;
+						}
+						IvParameterSpec ivSpec = new IvParameterSpec(Hex.convertHexToBytes(thingConfig.getIV()));
+						Map properties = editProperties();
+						byte decodedPayload[] = Utils.decrypt(Hex.fromHexString((String) properties.get("key")), ivSpec, Utils.slice(response, 56, 88));
+						if (decodedPayload == null) {
+								logError("Null payload in response from model 2 status request);
+								return false;
+						}
+						if (payload[4] == 1) {
+								updateState("powerOn", OnOffType.ON);
+						} else {
+								updateState("powerOn", OnOffType.OFF);
+						}
+						return true;
+				} catch (Exception ex) {
+						logError("Exception while getting status from device", ex);
+						return false;
+				}
     }
 
     public void updateItemStatus() {
         if (getStatusFromDevice()) {
-            if (!isOnline())
+            if (!isOnline()) {
                 updateStatus(ThingStatus.ONLINE);
-        } else if (!isOffline())
-            updateStatus(ThingStatus.OFFLINE, ThingStatusDetail.COMMUNICATION_ERROR, (new StringBuilder("Could not control device at IP address ")).append(thingConfig.getIpAddress()).toString());
+						}
+        } else if (!isOffline()) {
+            updateStatus(
+							ThingStatus.OFFLINE, 
+							ThingStatusDetail.COMMUNICATION_ERROR, 
+							"Could not control device at IP address " + thingConfig.getIpAddress()
+						)
+				}
     }
-
-    boolean powerON;
 }
